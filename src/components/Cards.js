4954d8e9877e0cd1f53827c8a531cc61b5d201d2
KanchanaSW <sk.personalusername@gmail.com>
2022-03-06 18:23:22 +0530

    done

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CardItem from "./CardItem";
-import './Cards.css'
+import "./Cards.css";
 
 function Cards() {
   return (
@@ -9,7 +9,38 @@ function Cards() {
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-          <CardItem />
+            <CardItem
+              src="images/img-9.jpg"
+              text="Explore the hidden waterfall deep inside the Amazon jungle."
+              label="Aventure"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Travel through the islands of Bali in a private cruise."
+              label="Travel"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/img-3.jpg"
+              text="Set sail in the Atlantic ocean visiting uncharted Waters."
+              label="Mystery"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-4.jpg"
+              text="Experiance Football on top of Hymilayan mountains."
+              label="Sport"
+              path="/products"
+            />
+            <CardItem
+              src="images/img-8.jpg"
+              text="Ride through the Sahara desert on a guided camel tour."
+              label="Travel"
+              path="/sign-up"
+            />
           </ul>
         </div>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,28 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
-import "./Navbar.css"
+import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [button,setButton] = useState(true);
+  const [button, setButton] = useState(true);
 
   const handelClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(!click);
 
-  const showButton = () =>{
-      if (window.innerWidth <= 960) {
-          setButton(false);
-      }else{
-          setButton(true);
-      }
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
   };
 
-  useEffect(() =>{
-      showButton();
-  },[]);
+  useEffect(() => {
+    showButton();
+  }, []);
 
-  window.addEventListener('resize' , showButton);
+  window.addEventListener("resize", showButton);
   return (
     <>
       <nav className="navbar">
@@ -58,12 +58,16 @@ function Navbar() {
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
                 Sign Up
               </Link>
             </li>
           </ul>
-          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </>
