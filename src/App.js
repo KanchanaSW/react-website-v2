84265389733281f3c4498d32f126669d463f5e61
KanchanaSW <sk.personalusername@gmail.com>
2022-03-06 11:45:18 +0530

    add card

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path='/' exact />
-        </Routes>
+        <Switch>
+        <Route path='/' component={Home}/>
+        </Switch>
       </Router>
     </>
   );
